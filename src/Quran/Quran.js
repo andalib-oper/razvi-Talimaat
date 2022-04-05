c4bb3514e97334d95388d4246b385ae3969a96b5
danish-ally <dannush786@gmail.com>
2022-04-05 21:00:48 +0530

    Adding surah apis in quran screen

--- src/Quran/Quran.js
+++ src/Quran/Quran.js
@@ -1,20 +1,64 @@
-import React from 'react';
-import {View,Text, StyleSheet} from 'react-native'
+// import React from 'react';
+// import {View,Text, StyleSheet} from 'react-native'
 
-const Quran = () =>{
-    return(
-        <View style={styles.container}>
+// const Quran = () =>{
+//     return(
+//         <View style={styles.container}>
+//             <Text>
+//                 Quran
+//             </Text>
+//         </View>
+//     )
+// }
+
+// const styles=StyleSheet.create({
+//     container: {
+//         flex: 1,
+//     },
+// })
+
+// export default Quran;
+
+import React, {useEffect, useState} from 'react';
+import {ActivityIndicator, FlatList, Text, View} from 'react-native';
+
+export default Quran = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getSurahs = async () => {
+    try {
+      const response = await fetch('http://api.alquran.cloud/v1/surah');
+      const json = await response.json();
+      setData(json.data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getSurahs();
+  }, []);
+
+  return (
+    <View style={{flex: 1, padding: 24}}>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={({id}, index) => id}
+          renderItem={({item}) => (
             <Text>
-                Quran
+              {item.number}, {item.name}, {item.englishName},{' '}
+              {item.englishNameTranslation}, {item.numberOfAyahs},{' '}
+              {item.revelationType}
             </Text>
-        </View>
-    )
-}
-
-const styles=StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-})
-
-export default Quran;
+          )}
+        />
+      )}
+    </View>
+  );
+};
--- src/Ramzan/Ramzan.js
+++ src/Ramzan/Ramzan.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import {View, Text} from 'react-native'
+import {View, Text, StyleSheet} from 'react-native';
 
-const Ramzan = () =>{
-    return(
-        <View>
-            <Text>
-                Ramzan
-            </Text>
-        </View>
-    )
-}
+const Ramzan = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Ramzan</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default Ramzan;
