e6f052df6e6079972dd804a598570f0a6396c08d
Andalib <andalibquraishi14@gmail.com>
2022-04-08 21:58:41 +0530

    Calendar

--- src/Calendar/Calendar.js
+++ src/Calendar/Calendar.js
@@ -1,141 +1,79 @@
-import React, {useState}from 'react';
-import {View, Text} from 'react-native'
-import { Agenda} from 'react-native-calendars';
+// import React, { useState } from 'react';
+// import { View, Text, StyleSheet } from 'react-native'
+// import { Calendar } from 'react-native-calendars';
 
-const Calendar = () =>{
-    
-    return(
-        <View>
-            <Text>
-                Calendar
-            </Text>
-            
-        </View>
-    )
+// const CalendarScreen = () => {
+
+//     return (
+//         <View style={styles.container}>
+//             <Text>
+//                 Calendar
+//             </Text>
+           
+//         </View>
+//     )
+// }
+
+// const styles=StyleSheet.create({
+//     container: {
+//         flex:1,
+//     }
+// })
+
+// export default CalendarScreen;
+
+import React, {useState, Fragment, useCallback, useMemo} from 'react';
+import {StyleSheet, View, ScrollView, Text, TouchableOpacity} from 'react-native';
+import {Calendar, CalendarProps} from 'react-native-calendars';
+
+
+const INITIAL_DATE = '2020-02-02';
+
+const CalendarScreen = () => {
+  const [selected, setSelected] = useState(INITIAL_DATE);
+
+//   const onDayPress: CalendarProps['onDayPress'] = useCallback(day => {
+//     setSelected(day.dateString);
+//   }, []);
+const onDayPress = (day)=> {
+    setSelected(day.dateString)
 }
 
-export default Calendar;
-
-// import React, {Component} from 'react';
-// import {Alert, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
-// import {Agenda, DateData, AgendaEntry, AgendaSchedule} from 'react-native-calendars';
-
-// interface State {
-//   items?: AgendaSchedule;
-// }
-
-// export default class AgendaScreen extends Component<State> {
-//   state: State = {
-//     items: undefined
-//   };
-
-//   render() {
-//     return (
-//       <Agenda
-        
-//         items={this.state.items}
-//         loadItemsForMonth={this.loadItems}
-//         selected={'2017-05-16'}
-//         renderItem={this.renderItem}
-//         renderEmptyDate={this.renderEmptyDate}
-//         rowHasChanged={this.rowHasChanged}
-//         showClosingKnob={true}
-//         // markingType={'period'}
-//         // markedDates={{
-//         //    '2017-05-08': {textColor: '#43515c'},
-//         //    '2017-05-09': {textColor: '#43515c'},
-//         //    '2017-05-14': {startingDay: true, endingDay: true, color: 'blue'},
-//         //    '2017-05-21': {startingDay: true, color: 'blue'},
-//         //    '2017-05-22': {endingDay: true, color: 'gray'},
-//         //    '2017-05-24': {startingDay: true, color: 'gray'},
-//         //    '2017-05-25': {color: 'gray'},
-//         //    '2017-05-26': {endingDay: true, color: 'gray'}}}
-//         // monthFormat={'yyyy'}
-//         // theme={{calendarBackground: 'red', agendaKnobColor: 'green'}}
-//         //renderDay={(day, item) => (<Text>{day ? day.day: 'item'}</Text>)}
-//         // hideExtraDays={false}
-//         // showOnlySelectedDayItems
-//       />
-//     );
-//   }
-
-//   loadItems = (day: DateData) => {
-//     const items = this.state.items || {};
-
-//     setTimeout(() => {
-//       for (let i = -15; i < 85; i++) {
-//         const time = day.timestamp + i * 24 * 60 * 60 * 1000;
-//         const strTime = this.timeToString(time);
-
-//         if (!items[strTime]) {
-//           items[strTime] = [];
-          
-//           const numItems = Math.floor(Math.random() * 3 + 1);
-//           for (let j = 0; j < numItems; j++) {
-//             items[strTime].push({
-//               name: 'Ramzan' + strTime + ' #' + j,
-//             //   height: Math.max(50, Math.floor(Math.random() * 150)),
-//             //   day: strTime
-//             });
-//           }
-//         }
-//       }
+  const marked = useMemo(() => {
+    return {
+      [selected]: {
+        selected: true,
+        disableTouchEvent: true,
+        selectedColor: 'blue',
+        selectedTextColor: 'white'
+      }
+    };
+  }, [selected]);
+  return(
+    <Fragment>
+    <Text style={styles.text}>Calendar with selectable date</Text>
+    <Calendar
       
-//       const newItems: AgendaSchedule = {};
-//       Object.keys(items).forEach(key => {
-//         newItems[key] = items[key];
-//       });
-//       this.setState({
-//         items: newItems
-//       });
-//     }, 1000);
-//   }
+      enableSwipeMonths
+      current={INITIAL_DATE}
+      style={styles.calendar}
+      onDayPress={onDayPress}
+      markedDates={marked}
+    />
+  </Fragment>
+  )
+}
 
-//   renderItem = (reservation: AgendaEntry, isFirst: boolean) => {
-//     const fontSize = isFirst ? 16 : 14;
-//     const color = isFirst ? 'black' : '#43515c';
+export default CalendarScreen;
 
-//     return (
-//       <TouchableOpacity
-
-//         style={[styles.item, {height: reservation.height}]}
-//         onPress={() => Alert.alert(reservation.name)}
-//       >
-//         <Text style={{fontSize, color}}>{reservation.name}</Text>
-//       </TouchableOpacity>
-//     );
-//   }
-
-//   renderEmptyDate = () => {
-//     return (
-//       <View style={styles.emptyDate}>
-//         <Text>This is empty date!</Text>
-//       </View>
-//     );
-//   }
-
-//   rowHasChanged = (r1: AgendaEntry, r2: AgendaEntry) => {
-//     return r1.name !== r2.name;
-//   }
-
-//   timeToString(time: number) {
-//     const date = new Date(time);
-//     return date.toISOString().split('T')[0];
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//   item: {
-//     backgroundColor: 'white',
-//     flex: 1,
-//     borderRadius: 5,
-//     padding: 10,
-//     marginRight: 10,
-//     marginTop: 17
-//   },
-//   emptyDate: {
-//     height: 15,
-//     flex: 1,
-//     paddingTop: 30
-//   }
-// });
+const styles = StyleSheet.create({
+  calendar: {
+    marginBottom: 10
+  },
+  text: {
+    textAlign: 'center',
+    padding: 10,
+    backgroundColor: 'lightgrey',
+    fontSize: 16
+  },
+});
